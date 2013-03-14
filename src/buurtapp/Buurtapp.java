18c8ac66ece7787f49fdead604a2b51f0bdfc90a
tim <tim@192.168.2.170>
2013-03-14 08:54:35 +0100

    aanmaken melding, melding DAO en anpassen main voor testen van database connectie en functies

--- src/buurtapp/Buurtapp.java
+++ src/buurtapp/Buurtapp.java
@@ -4,16 +4,40 @@
  */
 package buurtapp;
 
+import java.util.List;
+
 /**
  *
  * @author tim
  */
 public class Buurtapp {
 
-    /**
-     * @param args the command line arguments
-     */
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        MeldingDAO Mdao = MeldingDAO.getInstance();
+        
+        Melding m1 = new Melding (1, "vandalisme", "Graffiti op de muur gespoten.");
+        Melding m2 = new Melding (2, "onderhoud", "Een put in de weg.");
+        
+        Mdao.addMeldingen(m1,m2);
+        
+        List<Melding> allMeldingen = Mdao.getAllMeldingen();
+        for (Melding m : allMeldingen) {
+            System.out.printf("Melding gevonden met nummer: %d, type: %s en beschrijving: %s%n", m.getId(), m.getType(), m.getBeschrijving());
+        }
+        
+        m2.setBeschrijving("Een put in het wegdek.");
+        
+        Mdao.updateMelding(m2);
+        
+        allMeldingen = Mdao.getAllMeldingen();
+        for (Melding m : allMeldingen) {
+            System.out.printf("Melding gevonden met nummer: %d, type: %s en beschrijving: %s%n", m.getId(), m.getType(), m.getBeschrijving());
+        }
+        
+        Mdao.deleteMeldingen(m1,m2);       
+        
     }
 }
